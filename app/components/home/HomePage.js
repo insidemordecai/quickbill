d5c93016e2c614dc7cb324bfcb6b338a0434f01c
Mordecai K <mordecai.kipngetich@gmail.com>
2022-02-01 13:41:03 +0300

    update: home

--- app/components/home/HomePage.js
+++ app/components/home/HomePage.js
@@ -11,30 +11,6 @@ export default class HomePage extends Component {
                     <Link to="/QuickBill/dashboard" className="solid-btn">Create Invoice</Link>
                     <h4>No signup required</h4>
                 </div>
-                {/* <div className="home__banner home__banner--footer">
-                    <h4>Star this project on Github</h4>
-                    <div className="github-buttons">
-                        <a className="github-button"
-                            href="https://github.com/PunitGr/QuickBill"
-                            data-icon="octicon-star"
-                            data-size="large"
-                            data-show-count="true"
-                            aria-label="Star PunitGr/QuickBill on GitHub"
-                            alt="github-star">
-                                Star
-                        </a>
-                        <a className="github-button"
-                            href="https://github.com/PunitGr/QuickBill/fork"
-                            data-icon="octicon-repo-forked" data-size="large"
-                            data-show-count="true"
-                            aria-label="Fork PunitGr/QuickBill on GitHub"
-                            alt="github-fork">
-                                Fork
-                        </a>
-                    </div>
-
-                    <a href="mailto:groove67@gmail.com" className="solid-btn"><i className="fa fa-envelope" aria-hidden="true"></i>Hire me!</a>
-                </div> */}
             </div>
         );
     }
