0aaa2a8c2952c12c5901eda277c24eb55587c68d
itsmordecai <mordecai.kipngetich@gmail.com>
2022-02-01 14:35:54 +0300

    update: home and default currency

--- app/reducers/currencyReducer.js
+++ app/reducers/currencyReducer.js
@@ -4,7 +4,7 @@ import type { Action } from "../actions";
 
 type State = Object;
 
-export default function currencyReducer(state: State = { "value": "$", "label": "USD" }, action: Action) {
+export default function currencyReducer(state: State = { "value": "Ksh", "label": "KES" }, action: Action) {
     if (action.type === SET_CURRENCY) {
         return action.currency;
     }
