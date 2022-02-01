db11c366d50ad59f52bc2545e59f669eac29f7eb
itsmordecai <mordecai.kipngetich@gmail.com>
2022-02-01 14:46:59 +0300

    update: set default VAT

--- app/reducers/addInfoReducer.js
+++ app/reducers/addInfoReducer.js
@@ -9,7 +9,7 @@ type State = {
     vat?: string,
 };
 
-export default function addInfoReducer(state: State = {discount: "0", tax: "0", amountPaid: "0", vat: "0"}, action: Action):State {
+export default function addInfoReducer(state: State = {discount: "0", tax: "0", amountPaid: "0", vat: "16"}, action: Action):State {
     if (action.type === SET_ADDINFO) {
         return {
             discount: action.discount,
